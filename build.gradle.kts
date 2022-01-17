ff3d80da6a5d1cbcb37e193d61bdd2994715efa6
acrylic-style <me@acrylicstyle.xyz>
2022-01-17 17:55:53 +0900

    publish configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     java
     `java-library`
+    `maven-publish`
 }
 
 group = "net.azisaba"
@@ -17,12 +18,7 @@ subprojects {
     apply {
         plugin("java")
         plugin("java-library")
-    }
-
-    java {
-        toolchain {
-            languageVersion.set(JavaLanguageVersion.of(8))
-        }
+        plugin("maven-publish")
     }
 
     repositories {
@@ -39,6 +35,44 @@ subprojects {
     tasks {
         getByName<Test>("test") {
             useJUnitPlatform()
+        }
+    }
+}
+
+allprojects {
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(8))
+        }
+
+        withSourcesJar()
+        withJavadocJar()
+    }
+
+    val javaComponent = components["java"] as AdhocComponentWithVariants
+    javaComponent.withVariantsFromConfiguration(configurations["sourcesElements"]) {
+        skip()
+    }
+
+    publishing {
+        repositories {
+            maven {
+                name = "repo"
+                credentials(PasswordCredentials::class)
+                url = uri(
+                    if (project.version.toString().endsWith("SNAPSHOT"))
+                        project.findProperty("deploySnapshotURL") ?: System.getProperty("deploySnapshotURL", "")
+                    else
+                        project.findProperty("deployReleasesURL") ?: System.getProperty("deployReleasesURL", "")
+                )
+            }
+        }
+
+        publications {
+            create<MavenPublication>("mavenJava") {
+                from(components["java"])
+                artifact(tasks.getByName("sourcesJar"))
+            }
         }
     }
 }
